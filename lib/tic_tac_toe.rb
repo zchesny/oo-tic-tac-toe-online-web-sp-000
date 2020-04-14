9a58bdcba0bc628165defbb7c9ea98ec6af1f6af
Zoey Chesny <z.chesny@gmail.com>
2020-04-14 20:09:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ class TicTacToe
     WIN_COMBINATIONS.detect do |combo|
       @board[combo[0]] == @board[combo[1]] &&
       @board[combo[1]] == @board[combo[2]] &&
-      position_taken?(@board, combo[0])
+      position_taken?(combo[0])
     end
   end
 
