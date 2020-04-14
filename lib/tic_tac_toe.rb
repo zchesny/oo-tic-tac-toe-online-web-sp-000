9b60cd73e0182430b633272c82db9edb6b3245ac
Zoey Chesny <z.chesny@gmail.com>
2020-04-14 20:08:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(@board, index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn_count
